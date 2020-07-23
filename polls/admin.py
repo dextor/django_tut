1ca92c8334fc9d6a4b379de1b37c6c7bc841e074
Dexter Wayne <dxtrwyn@gmail.com>
2020-07-23 16:06:41 -0600

    Question model for admin update

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,11 @@ from .models import Question
 from .models import Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+# 
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
